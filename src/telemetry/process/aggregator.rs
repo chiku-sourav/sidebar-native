de3cc3cdc2f665c48a6d23e3add22bad1ba68e41
Sourav Ghosh <sourav011990@gmail.com>
2026-08-31 19:15:08 +0530

    fix: improve disk sorting logic in aggregate_and_rank_processes function

--- src/telemetry/process/aggregator.rs
+++ src/telemetry/process/aggregator.rs
@@ -72,8 +72,7 @@ pub fn aggregate_and_rank_processes(
 
     // 3. Top Disk
     let mut disk_list = base_list.clone();
-    disk_list
-        .sort_by(|a, b| (b.1.disk_read + b.1.disk_write).cmp(&(a.1.disk_read + a.1.disk_write)));
+    disk_list.sort_by_key(|a| std::cmp::Reverse(a.1.disk_read + a.1.disk_write));
     let disk = disk_list
         .into_iter()
         .take(limit)
